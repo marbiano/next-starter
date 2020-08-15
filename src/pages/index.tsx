47866f3c5b2e2ea947188ac96d091da007720fa7
marbiano <martin@Martins-MacBook-Pro.local>
2020-08-15 00:20:00 -0300

    Add basic structure for pages and empty shell for lib

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,15 @@
-export default function Home() {
-  return <div>Hello, fellow Nextronaut!</div>;
-}
+import { GetStaticProps, GetStaticPaths } from 'next';
+
+interface PageProps {}
+
+const IndexPage: React.FC<PageProps> = () => {
+  return <h1>Hello, fellow Nextronaut!</h1>;
+};
+
+export const getStaticProps: GetStaticProps = async (context) => {
+  return {
+    props: {},
+  };
+};
+
+export default IndexPage;
